a5e20cc46977953407a09709016cafbe9503a38f
cultured-sequence-b2a8 <britt90609@gmail.com>
2021-05-05 21:35:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,11 @@ introduction(name)
 name = "Dan"
 language = "Ember.js"
 
+<<<<<<< HEAD
 def introduction_with_language(name, language)
+=======
+def introduction_with_name(name, language)
+>>>>>>> c5e4975d9f848d861d644bff6f6cb760ac0af1f4
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
